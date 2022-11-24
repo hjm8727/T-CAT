d2c7ec5670ca52deb86ed9e248f98379df984229
jimin0601 <jimin600155@naver.com>
2022-11-24 15:49:55 +0900

    jm

--- src/components/views/DetailPage/Section/Summary/Poster.js
+++ src/components/views/DetailPage/Section/Summary/Poster.js
@@ -4,13 +4,13 @@ import Star from './Star';
 function Poster() {
     return (
     <div>        
-    <h3 className='summary-top'>태양의서커스 ＜뉴 알레그리아＞</h3>
+    <h2 className='summary-top'>태양의서커스 ＜뉴 알레그리아＞</h2>
     <div className='summary-body'>
         <div className='poster-box' style={{margin: '0'}}>
             <img className='poster-box-top' src='test.gif' alt='포스터 이미지'/>
         <div className='poster-box-bottom'>
+        <span className='like' onClick={(() => {alert("성공")})}>❤</span>
             <Star/> <span>9.5</span>
-        <div className='like' onClick={(() => {alert("성공")})}>❤</div>
     </div>
     </div>
     </div></div>
