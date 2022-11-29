bd721bd398c6366d35eb7177f89624319d789d63
jimin0601 <jimin600155@naver.com>
2022-11-29 18:09:31 +0900

    11.29

--- src/components/views/AdminPage/Inquiry.js
+++ src/components/views/AdminPage/Inquiry.js
@@ -1,19 +1,67 @@
+import { useState } from "react";
 import styled from "styled-components";
 import NavBar from "./Tool/TopBar";
 
 const InquiryBlock=styled.div`
-  .container{
+margin:0 auto;
+  box-sizing: border-box;
+  width: 100vw;
+
+  .container {
+    margin : 10px;
+    display: flex;
     border: 1px solid black;
+    height: 60%;
+    flex-direction: column;
+    text-align: center;
+    padding: 3rem;
   }
+table,th,td {
+  border: 1px solid black;
+}
 `;
 
+
 const Inquiry=()=>{
+  const [open, setOpen] = useState(false);
+  
+  const Test = () => {
     return(
-        <InquiryBlock style={{width : '100vw'}}>
+      <div>
+        <h1>답장하기 테스트</h1>
+      </div>
+    );
+  }
+  const onClick2 = () => {
+    setOpen(!open);
+  }
+  return(
+    <InquiryBlock>
         <NavBar name="큐앤에이 관리"/>
           <div className="container">
-              
-          </div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>번호</th>
+                    <th>제목</th>
+                    <th>작성자</th>
+                    <th>작성일</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>환불해주세요 제발요</td>
+                    <td>rin777</td>
+                    <td>2022.11.23</td>
+                    <td><button type="button" onClick={onClick2}>답장하기</button></td>
+                </tr>
+                </tbody>
+              </table> 
+              <div>
+                {open && <Test />}
+              </div>
+            </div>
         </InquiryBlock>
     );
 
