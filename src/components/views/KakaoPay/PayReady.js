b2da1018b9886eb96de3a2e880c01cbfc6caceb8
jimin0601 <jimin600155@naver.com>
2022-12-02 12:38:21 +0900

    12.02 jm

--- src/components/views/KakaoPay/PayReady.js
+++ src/components/views/KakaoPay/PayReady.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
 
-const PayReady = props => {
+const PayReady = (item_name, total) => {
     let [data, setData] = useState({
         next_redirect_pc_url: "",
         tid: "",
@@ -13,13 +13,13 @@ const PayReady = props => {
             // 가맹점 회원 id
             partner_user_id: "partner_user_id",
             // 상품 이름
-            // item_name: props.item_name,
-            item_name: "태양의서커스 <뉴 알레그리아> (VIP석)",
+            item_name: item_name,
+            // item_name: "태양의서커스 <뉴 알레그리아> (VIP석)",
             // 상품 수량
             quantity: 1,
             // 총 가격
-            total_amount: 300000,
-            // total_amount: props.money,
+            // total_amount: 300000,
+            total_amount: total,
             // 상품 비과세
             tax_free_amount: 20000,
             // 결제 성공 URL
@@ -56,11 +56,10 @@ const PayReady = props => {
         })
     })
     const { next_redirect_pc_url } = data;
-    // https://online-pay.kakao.com/mockup/v1/a53868020cba93a89c3123cbfeddaa339a386af3a5675ac0b1ad481d7a73211c/info
     return (
             <div className="container">
-                <h2 className="head-line">카카오페이 결제하기</h2>
-                <a href={ next_redirect_pc_url }><img className="kakao" src="../images/test.png" alt="카카오페이" /></a>
+                {/* <h2 className="head-line">카카오페이 결제하기</h2>
+                <a href={ next_redirect_pc_url }><img className="kakao" src="../images/test.png" alt="카카오페이" /></a> */}
             </div>
         )
 }
